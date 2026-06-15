c821743fe4ea419c1a33ee7134b37964da8c6270
Ivan Izobov <izobovivan@gmail.com>
2026-06-16 02:58:49 +0300

    feat: add root layout with fonts and global styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '@fontsource/playfair-display/latin-500-italic.css';
+	import '@fontsource/playfair-display/latin-600.css';
+	import '@fontsource/dm-sans/latin-400.css';
+	import '@fontsource/dm-sans/latin-500.css';
+	import '@fontsource/dm-sans/latin-700.css';
+	import '$lib/styles/global.scss';
+	import { onMount } from 'svelte';
+	import { initSmoothScroll } from '$lib/motion/smoothScroll';
+
+	let { children } = $props();
+
+	onMount(() => initSmoothScroll());
+</script>
+
+{@render children()}

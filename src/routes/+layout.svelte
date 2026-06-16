7813139d5c06ba286579596a7cb29546a5710f5a
Ivan Izobov <izobovivan@gmail.com>
2026-06-16 08:08:37 +0300

    fix: import Lenis CSS so smooth-scroll actually scrolls

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import '@fontsource/dm-sans/latin-400.css';
 	import '@fontsource/dm-sans/latin-500.css';
 	import '@fontsource/dm-sans/latin-700.css';
+	import 'lenis/dist/lenis.css';
 	import '$lib/styles/global.scss';
 	import { onMount } from 'svelte';
 	import { initSmoothScroll } from '$lib/motion/smoothScroll';
